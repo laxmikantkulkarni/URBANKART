ced6ca4ce1f18edecf6780d75607bf9a3755f1e2
Atul Patankar <109102940+AtulPatankar5@users.noreply.github.com>
2023-08-20 23:23:56 +0530

    Delete ECommerce directory

--- ECommerce/src/main/java/com/app/custom_excceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.custom_excceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-	public ResourceNotFoundException(String mesg) {
-		super(mesg);
-	}//we have given RuntimeException as all spring exception are unchecked
-}
--- ECommerce/src/main/java/com/app/entities/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.app.entities;
-
-public enum OrderStatus {
-	SHIPPED, OUTFORDELIVERY, DELIVERED
-}
